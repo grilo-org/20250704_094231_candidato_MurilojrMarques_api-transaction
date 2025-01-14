43f4fef9057c7a007b703d4be0169e850153a66a
Murilo <muriloj2702@gmail.com>
2025-01-13 22:04:28 -0300

    feat: adicionar regras no usecase para validação de descrição e valor

--- usecase/transactionUsecase.go
+++ usecase/transactionUsecase.go
@@ -1,6 +1,9 @@
 package usecase
 
 import (
+	"fmt"
+	"math"
+
 	"github.com/MurilojrMarques/api-transaction.git/model"
 	"github.com/MurilojrMarques/api-transaction.git/repository"
 )
@@ -16,9 +19,19 @@ func NewTransactionUsecase(repo repository.TransactionRepository) TransactionUse
 }
 
 func (tu *TransactionUsecase) CreateTransaction(transaction model.Transaction) (model.Transaction, error) {
+	if len(transaction.Description) > 50 {
+		return model.Transaction{}, fmt.Errorf("a descrição não pode exceder 50 caracteres")
+	}
+
+	if transaction.Value <= 0 {
+		return model.Transaction{}, fmt.Errorf("o valor da compra deve ser positivo")
+	}
+
+	transaction.Value = math.Round(transaction.Value*100) / 100
+
 	transactionId, err := tu.repository.CreateTransaction(transaction)
 	if err != nil {
-		return model.Transaction{}, err
+		return model.Transaction{}, fmt.Errorf("erro ao criar transação: %s", err.Error())
 	}
 
 	transaction.ID = transactionId
