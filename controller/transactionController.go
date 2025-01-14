7b7a86d4b3597c3b87a075f99fecd05be652ebfb
Murilo <muriloj2702@gmail.com>
2025-01-13 22:04:48 -0300

    fix: corrigir mensagens de erro no controller

--- controller/transactionController.go
+++ controller/transactionController.go
@@ -22,14 +22,14 @@ func (t *transactionController) CreateTransaction(ctx *gin.Context) {
 	var transaction model.Transaction
 	err := ctx.BindJSON(&transaction)
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, err)
+		ctx.JSON(http.StatusBadRequest, gin.H{"errors": "Dados inválidos"})
 		return
 	}
 
 	insertedTransaction, err := t.transactionUsecase.CreateTransaction(transaction)
 
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, err)
+		ctx.JSON(http.StatusInternalServerError, gin.H{"erros": err.Error()})
 		return
 	}
 
